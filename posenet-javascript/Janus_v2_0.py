395cf5222b42459057ec37ee27a0fbcc8d14163e
jamesperalta <jamesperalta35@gmail.com>
2020-03-02 14:49:31 -0700

    Added rep counting to the driver using the Janus v2.0 library

---
+++ posenet-javascript/Janus_v2_0.py
@@ -0,0 +1,159 @@
+import cv2
+import configparser
+import requests
+import json
+
+# API Endpoint
+URL = "http://localhost:3000/python/posenet"
+headers = {'content-type': "image/jpeg"}
+
+# Load in configuration
+config = configparser.ConfigParser()
+config.read('configurations.cfg')
+
+keypoints_list = [
+    "nose", #1
+    "leftEye", #2
+    "rightEye", #3
+    "leftEar", #4
+    "rightEar", #5
+    "leftShoulder", #6
+    "rightShoulder", #7
+    "leftElbow", #8
+    "rightElbow", #9
+    "leftWrist", #10
+    "rightWrist", #11
+    "leftHip", #12
+    "rightHip", #13
+    "leftKnee", #14
+    "rightKnee", #15
+    "leftAnkle", #16
+    "rightAnkle", #17
+]
+
+radius = 3
+color = (255, 0, 0)
+thickness = 3
+
+class Janus:
+    def __init__(self, stream):
+        self.cap = stream
+
+        # These variables are used to calculate
+        # the rate of change for counting reps
+        self.count = 0
+        self.past = None
+        self.roc_sampling = int(config.get("Algorithm", "ROC_Sampling"))
+        self.up_rep = False
+        self.down_rep = False
+        self.rep_count = 0
+
+        # Remebering what Janus has seen so far
+        self.keypoints_detected = []
+        self.people_detected = []
+        # self.all_keypoints_detected = []
+
+    def get_poses(self):
+        # Capture frame-by-frame
+        ret, frame = self.cap.read()
+        height = int(config.get("VideoSize", "Height"))
+        width = int(config.get("VideoSize", "Width"))
+        frame = cv2.resize(frame, (height, width))
+        en_ret, en_frame = cv2.imencode('.jpg', frame)
+
+        keypoints, people_location = self.get_keypoints(en_frame)
+
+        frame = self.draw_coordinates(keypoints, frame)
+
+        # TODO: ACTUALLY IMPLEMENT THIS VARIABLES
+        num_keypoints_detected = 16
+        num_of_people_detected = 1
+
+        return frame, num_keypoints_detected, people_location, num_of_people_detected
+
+    def count_reps(self, people_location):
+        if self.past is None:
+            self.past = people_location
+        elif self.count % self.roc_sampling == 0:
+            present = people_location
+
+            # Calculate the rate of change
+            result = self.rate_of_change(self.past, present)
+
+            if result == "Up":
+                self.up_rep = True
+            elif result == "Down":
+                self.down_rep = True
+            else:
+                if self.up_rep is True and self.down_rep is True:
+                    self.rep_count += 1
+                    self.up_rep = False
+                    self.down_rep = False
+
+            # Reset variables
+            self.past = None
+            self.count = -1
+
+        self.count += 1
+
+        return self.rep_count
+
+    def rate_of_change(self, past_keypoints, present_keypoints):
+        increasing = False
+        decreasing = False
+
+        for index in range(0, len(keypoints_list)):
+            # Retrieve datapoint for a body part
+            past = past_keypoints[index][1]
+            present = present_keypoints[index][1]
+
+            # Calculate growth rates
+            growth_rate = (present - past) / past
+            if growth_rate > 0.1:
+                increasing = True
+                break
+
+            # Calculate decay rates
+            decay_rate = (past - present) / present
+            if decay_rate > 0.1:
+                decreasing = True
+                break
+
+        if increasing is False and decreasing is False:
+            return "Still"
+        elif increasing is True:
+            return "Up"
+        else:
+            return "Down"
+
+    def get_keypoints(self, img):
+        r = requests.post(url=URL, data=img.tostring(), headers=headers)
+        json_file = json.loads(r.text)
+
+        # Get num of keypoints
+        body_point_map = {}
+        people_location = []
+        for i in json_file[0]['keypoints']:
+            part = i["part"]
+            x_coord = i["position"]["x"]
+            y_coord = i["position"]["y"]
+
+            body_point_map[part] = (x_coord, y_coord)
+            people_location.append([x_coord, y_coord])
+
+        return body_point_map, people_location
+
+    def draw_coordinates(self, keypoints, img):
+        # Draw a circle with blue line borders of thickness of 2 px
+        for i in keypoints.keys():
+            x_coord = int(keypoints[i][0])
+            y_coord = int(keypoints[i][1])
+            img = cv2.circle(img, (x_coord, y_coord), radius, color, thickness)
+
+        return img
+
+    def get_model_config(self):
+        response = requests.get(url=URL + "/modelconfig")
+        response_json = json.loads(response.text)["config"]
+
+        return response_json
--- posenet-javascript/driver.py
+++ posenet-javascript/driver.py
@@ -1,57 +1,51 @@
 import cv2
 import requests
 import json
+import Janus_v2_0
 
 # API Endpoint
 URL = "http://localhost:3000/python/posenet"
 headers = {'content-type': "image/jpeg"}
-# cap = cv2.VideoCapture("./squat10.mp4")
+# cap = cv2.VideoCapture("./squat_0.mp4")
 cap = cv2.VideoCapture(0)
+janus = Janus_v2_0.Janus(cap)
 
-radius = 3
-color = (255, 0, 0)
-thickness = 3
-
-
-def get_keypoints(img):
-    r = requests.post(url=URL, data=img.tostring(), headers=headers)
-    json_file = json.loads(r.text)
-
-    body_point_map = {}
-    for i in json_file[0]['keypoints']:
-        part = i["part"]
-        x_coord = i["position"]["x"]
-        y_coord = i["position"]["y"]
-
-        body_point_map[part] = (x_coord, y_coord)
-
-    return body_point_map
-
-
-def draw_coordinates(keypoints, img):
-    # Draw a circle with blue line borders of thickness of 2 px
-    for i in keypoints.keys():
-        x_coord = int(keypoints[i][0])
-        y_coord = int(keypoints[i][1])
-        img = cv2.circle(img, (x_coord, y_coord), radius, color, thickness)
-
-    return img
-
-
+counting_reps = False
+rep_count = 0
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    frame = cv2.resize(frame, (500, 500))
-    en_ret, en_frame = cv2.imencode('.jpg', frame)
 
-    keypoints = get_keypoints(en_frame)
-    frame = draw_coordinates(keypoints, frame)
+    # Get and draw poses
+    frame, num_keypoints_detected, people_location, num_of_people_detected = janus.get_poses()
 
     frame = cv2.resize(frame, (500, 500))
-    # Display the resulting frame
+    if (counting_reps):
+        # Use PoseNet outout to detect repetitons
+        rep_count = janus.count_reps(people_location)
+
+        # Reporting workout and rep count
+        frame = cv2.rectangle(frame, (20 - 15, 380 + 35), (145 - 15, 440 + 35), (255, 255, 255), -1)
+        prediction = "Jahnus v2.0"  # posenet.detect_workout_type(i)
+        cv2.putText(frame, "{}".format(prediction), (8, 395 + 40), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(frame, 'Rep count: {}'.format(rep_count), (8, 420 + 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    (0, 0, 0), 1, cv2.LINE_AA)
+
     cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+
+    key_pressed = cv2.waitKey(1)
+    if key_pressed & 0xFF == ord('q'):
         break
+    # Hit "s" key
+    if key_pressed == 115:
+        if counting_reps:
+            print("Stop counting reps")
+            counting_reps = False
+            rep_count = 0
+        else:
+            print("Start counting reps")
+            counting_reps = True
 
 # When everything done, release the capture
 cap.release()
