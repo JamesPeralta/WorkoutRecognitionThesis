cd9561857016f3e972e009614c2443ee6f09bc28
jamesperalta <jamesperalta35@gmail.com>
2020-03-30 21:21:46 -0600

    Modulated code for running optical flow

--- posenet-javascript/Optical_Flow.py
+++ posenet-javascript/Optical_Flow.py
@@ -3,6 +3,8 @@ import numpy as np
 
 
 video_size = (500, 500)
+# Parameters for Lucas-Kanade optical flow
+lk_params = dict(winSize=(15, 15), maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
 
 def generate_sparse_features():
@@ -17,94 +19,87 @@ def generate_sparse_features():
     return np.array(np.meshgrid(x_vector, y_vector)).T.reshape(-1, 1, 2)
 
 
-def find_displaced_features(prev, next):
+def find_displaced_features(prev_f, next_f):
     # Find displaced features with respect to y
-    diff_arr = np.subtract(prev, next)
+    diff_arr = np.subtract(prev_f, next_f)
     diff_arr_y = diff_arr[:, 1]
 
-    increasing = np.argwhere(diff_arr_y > 2)[:, 0]
-    if len(increasing) > 20:
+    increasing = np.argwhere(diff_arr_y > 5)[:, 0]
+    if len(increasing) < 75:
+        increasing = np.array([])
+
+    decreasing = np.argwhere(diff_arr_y < -5)[:, 0]
+    if len(decreasing) < 75:
+        decreasing = np.array([])
+
+    increasing_size = increasing.size
+    decreasing_size = decreasing.size
+
+    if increasing_size == decreasing_size:
+        # print("Stationary")
+        return []
+    elif increasing_size > decreasing_size:
         print("Increasing")
         return increasing
-
-    decreasing = np.argwhere(diff_arr_y < -2)[:, 0]
-    if len(decreasing) > 20:
+    else:
         print("Decreasing")
         return decreasing
 
-    print("Stationary")
-    return []
+
+def calculate_optical_flow(prev_t, next_t, frame=None):
+    # Convert these to gray because we only need luminisence layer
+    prev_gray = cv2.cvtColor(prev_t, cv2.COLOR_BGR2GRAY)
+    next_gray = cv2.cvtColor(next_t, cv2.COLOR_BGR2GRAY)
+
+    # Generate a dense feature set F(t-1) for prev frame
+    prev_features = generate_sparse_features()
+
+    # Compute the Optical flow for frame t using F(t-1), this generates
+    # F(t) and status tells us if a feature couldn't be found in frame t
+    next_features, status, error = cv2.calcOpticalFlowPyrLK(prev_gray, next_gray, prev_features, None, **lk_params)
+
+    # Selects features that have moved
+    good_features_t_1 = prev_features[status == 1]  # Features at t-1
+    good_features_t = next_features[status == 1]  # Features a t
+
+    moved_indices = find_displaced_features(good_features_t_1, good_features_t)
+
+    if frame is None:
+        return
+
+    # Only draw features that have moved
+    for i, (new, old) in enumerate(zip(good_features_t_1[moved_indices], good_features_t[moved_indices])):
+        # Returns a contiguous flattened array as (x, y) coordinates for new point
+        a, b = new.ravel()
+
+        # Draws filled circle (thickness of -1) at new position with green color and radius of 3
+        frame = cv2.circle(frame, (a, b), 3, color, -1)
+
+    return frame
 
 
-cap = cv2.VideoCapture("./ohp6.mp4")
+cap = cv2.VideoCapture("./ohp35.mp4")
+# cap = cv2.VideoCapture(1)
 video_size = (500, 500)
 # cap = cv2.VideoCapture(0)
-
-# Parameters for Shi-Tomasi corner detection
-feature_params = dict(maxCorners=300, qualityLevel=0.2, minDistance=2, blockSize=7)
-
-# Parameters for Lucas-Kanade optical flow
-lk_params = dict(winSize=(15, 15), maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
 # Variable for color to draw optical flow track
 color = (0, 255, 0)
 
-ret, first_frame = cap.read()
-first_frame = cv2.resize(first_frame, video_size)
-prev_gray = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
-
-prev = None
-# print(prev[:10].dtype)
-# print(" _________________ ")
-# prev = cv2.goodFeaturesToTrack(prev_gray, mask = None, **feature_params)
-# print(prev[:10].dtype)
-
-mask = np.zeros_like(first_frame)
-
-frame_on = 0
+ret, prev_frame = cap.read()
+prev_frame = cv2.resize(prev_frame, video_size)
 while cap.isOpened():
-    prev = generate_sparse_features()
-
-    frame_on = (frame_on + 1) % 5
-
     ret, next_frame = cap.read()
     next_frame = cv2.resize(next_frame, video_size)
+    raw_next_frame = next_frame  # Store the raw next frame for later
 
-    # Only need the luminance channel for detecting edges - less computationally expensive
-    gray = cv2.cvtColor(next_frame, cv2.COLOR_BGR2GRAY)
-
-    # Take in prev features and compute where they are next which is return in "next"
-    next, status, error = cv2.calcOpticalFlowPyrLK(prev_gray, gray, prev, None, **lk_params)
-
-    # Selects features that have moved
-    good_old = prev[status == 1]
-    good_new = next[status == 1]
-
-    moved_indices = find_displaced_features(good_old, good_new)
-
-    # Only draw features that have moved
-    for i, (new, old) in enumerate(zip(good_new[moved_indices], good_old[moved_indices])):
-        # Returns a contiguous flattened array as (x, y) coordinates for new point
-        a, b = new.ravel()
-        # Returns a contiguous flattened array as (x, y) coordinates for old point
-        c, d = old.ravel()
-        # Draws line between new and old position with green color and 2 thickness
-        # mask = cv2.line(mask, (a, b), (c, d), color, 2)
-        # Draws filled circle (thickness of -1) at new position with green color and radius of 3
-        next_frame = cv2.circle(next_frame, (a, b), 3, color, -1)
-    # Overlays the optical flow tracks on the original frame
-    # output = cv2.add(next_frame)
+    next_frame = calculate_optical_flow(prev_frame, next_frame, raw_next_frame)
 
     # Display the resulting frame
     cv2.imshow('frame', next_frame)
 
-    # Sets the new previous frames
-    prev_gray = gray.copy()
-
-    # Updates the features to ones that can still be found
-    prev = good_new.reshape(-1, 1, 2)
-
-    key_pressed = cv2.waitKey(30)
+    prev_frame = raw_next_frame
+    key_pressed = cv2.waitKey(50)
     if key_pressed & 0xFF == ord('q'):
         break
 
